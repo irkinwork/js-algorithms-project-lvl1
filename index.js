58df1d46816184226b2b2a8cbdb9734ae0ebc3cd
Irina <irin.work42@gmail.com>
2021-05-06 11:42:45 +0300

    Add reversed index

--- index.js
+++ index.js
@@ -2,23 +2,43 @@
 
 const getTerms = (text) => text.match(/\w+/g);
 
-const getFuzzySearch = (doc, query) => {
-    const terms = getTerms(doc.text);
-    const queryTerms = getTerms(query);
-    const result = terms.reduce((acc, item) => {
-        const count = queryTerms.includes(item) ? acc.count + 1 : acc.count;
-        return {...acc, query, count}
-    }, {count: 0, ...doc});
-    return result;
+const buildTermValue = (prev = [], id) => {
+    const prevIdValue = prev.find(item => item.id === id);
+    if (!prevIdValue) {
+        return [...prev, {id, count: 1}];
+    }
+    const nextIdValue = {...prevIdValue, count: prevIdValue.count + 1};
+    return prev.map(item => item.id === id ? nextIdValue : item);
+}
+
+ const buildReversedIndex = (docs) => {
+    const dic = docs.flatMap(doc => getTerms(doc.text).map(item => ({term: item, id: doc.id})))
+     return dic.reduce((acc, item) => {
+        const {term, id} = item;
+        const prevTermValue = acc[term];
+        const nextTermValue = buildTermValue(prevTermValue, id)
+        return {...acc, [term]: nextTermValue}
+    }, {})
  }
 
 const relevantSort = (search1, search2) => search2.count - search1.count;
 
+const reduceCount = (acc, item) => {
+    const {id, count} = item;
+    const prevIdItem = acc.find(item => item.id === id);
+    if (!prevIdItem) {
+        return [...acc, {id, count}]
+    }
+    const nextIdItem = {...prevIdItem, count: prevIdItem.count + count};
+    return acc.map(item => item.id === id ? nextIdItem : item);
+}
+
 export default (docs) => {
+    const dic = buildReversedIndex(docs);
     const search = (word) => {
-        return docs
-            .map((doc) => getFuzzySearch(doc, word))
-            .filter(search => search.count > 0)
+        const queryTerms = getTerms(word);
+        const docsCount = queryTerms.flatMap(term => dic[term]).reduce(reduceCount, []);
+        return docsCount
             .sort(relevantSort)
             .map(search => search.id);
     }
